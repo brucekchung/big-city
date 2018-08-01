26c379c61992c9ba88efeda634d6b153bd8d14ab
Bruce Chung <brucekchung@gmail.com>
2018-08-01 08:55:46 -0600

    Basic structure added for all components except footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,9 @@ import './App.css'
 import Banner from '../Banner/Banner'
 import Description from '../Description/Description'
 import Photos from '../Photos/Photos'
+import Sales from '../Sales/Sales'
+import Instructions from '../Instructions/Instructions'
+
 
 class App extends Component {
   render() {
@@ -11,6 +14,8 @@ class App extends Component {
         <Banner />
         <Description />
         <Photos />
+        <Sales />
+        <Instructions />
       </div>
     )
   }
---
+++ src/components/Sales/Sales.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Sales.css'
+
+const Sales = () => (
+  <div className="Sales">
+    <div className="goal">
+      <h4>Goal: $100,000</h4>
+      <p>diagram</p>
+    </div>
+    <div className="purchase">
+      <button>Tickets $30</button>
+      <p>Last day: Month Day</p>
+      <p>time remaining: countdown</p>
+    </div>
+  </div>
+)
+
+export default Sales
